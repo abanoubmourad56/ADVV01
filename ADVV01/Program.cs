f48c05ec6229f210d79b0e138f238e1d4b64d0ab
abanoubmourad99-crypt <abanoubmourad99@gmail.com>
2026-08-23 16:39:10 +0300

    question 2

--- ADVV01/Program.cs
+++ ADVV01/Program.cs
@@ -6,6 +6,22 @@
         {
             // Q1: What is a generic class? Why use generics?
             //A generic class works with different data types.Generics provide type safety and code reusability.
-        }
+
+            //Q2: Write a generic class Container<T> with Add and Get methods.
+        //    class Container<T>
+        //{
+        //    private T value;
+
+        //    public void Add(T item)
+        //    {
+        //        value = item;
+        //    }
+
+        //    public T Get()
+        //    {
+        //        return value;
+        //    }
+        //}
+    }
     }
 }
