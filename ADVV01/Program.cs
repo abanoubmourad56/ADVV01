71f44209664393b2426d90ab49bcc64c7e24d6a8
abanoubmourad99-crypt <abanoubmourad99@gmail.com>
2026-08-23 16:25:21 +0300

    Q1

--- ADVV01/Program.cs
+++ ADVV01/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Q1: What is a generic class? Why use generics?
+            //A generic class works with different data types.Generics provide type safety and code reusability.
         }
     }
 }
